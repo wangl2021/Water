bfe3eddb380cc9f831e53ac5bcf9a56b4e2e1a96
wanglei <13261828313@163.com>
2021-02-01 17:37:22 +0800

    add  sqlinfo

---
+++ watermelon/migrations/SqlInfo.py
@@ -0,0 +1,29 @@
+import pymysql
+def loginsql(env):
+    db = {
+        'local': ['localhost', 'root', 'root'],
+    }
+# 打开数据库连接
+    db = pymysql.connect(
+        host=db[env][0],
+        user=db[env][1],
+        password=db[env][2],
+        database="qa",
+        port=3306,
+        charset='utf8',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    return db
+def sqlInfo (env,sql,flag):
+    db = loginsql(env)
+    if flag == 1:
+        curson=db.cursor()
+        curson.execute(sql)
+        info = curson.fetchall()
+        print(info)
+        return info
+
+#sqlInfo('local', 'select * from peony', 1)
+
+
+
